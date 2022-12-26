c4ab678e5ec1099e0fabf242f362acbfdbf985c6
which-know-return <zzz2920408616@163.com>
2022-12-26 16:28:42 +0800

    add

--- 算法题练习/2022122502.cpp
+++ 算法题练习/2022122502.cpp
@@ -6,22 +6,25 @@ bool used[N];
 int n, m;
 
 void dfs(int u) {
-	if (u > n) {
-		for (int i = 1; i <= n; i++) {
+	if (u > m) {
+		for (int i = 1; i <= m; ++i) {
 			cout << st[i] << ' ';
 		}
 		cout << endl;
 		return;
 	}
-	for (int i = 1; i <= n; i++) {
-		if (!used[u]) {
-			st[i] = u;
-			used[u] = true;
+	for (int i = 1; i <= n; ++i) {
+		if (!used[i]) {
+//			if (st[u - 1] > i)
+//				return;
+			st[u] = i;
+			used[i] = true;
 			dfs(u + 1);
-			st[i] = 0;
-			used[u] = false;
+			st[u] = 0;
+			used[i] = false;
 		}
 	}
+
 }
 
 int main() {
